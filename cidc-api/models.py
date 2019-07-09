35fedd23ac8749d417d6e967ad70d5b268af4b4f
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-07-09 12:04:58 -0400

    Initial metadata patching function

--- cidc-api/models.py
+++ cidc-api/models.py
@@ -34,17 +34,44 @@ class Users(CommonColumns):
             for the given email.
         """
         session = app.data.driver.session
-        if not session.query(Users).filter_by(email=email).first():
+        if not session.query(cls).filter_by(email=email).first():
             app.logger.info(f"Creating new user with email {email}")
-            session.add(Users(email=email))
+            session.add(cls(email=email))
             session.commit()
 
 
 class TrialMetadata(CommonColumns):
     __tablename__ = "trial_metadata"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    study_id = Column(Integer, unique=True, nullable=False, index=True)
+    # The CIMAC-determined trial id
+    trial_id = Column(Integer, unique=True, nullable=False, index=True)
     metadata_json = Column(JSONB, nullable=False)
 
     # Create a GIN index on the metadata JSON blobs
     _metadata_idx = Index("metadata_idx", metadata_json, postgresql_using="gin")
+
+    @classmethod
+    def patch_trial_metadata(cls, trial_id: str, metadata: dict):
+        """
+            Applies updates to an existing trial metadata record,
+            or create a new one if it does not exist.
+
+            Args:
+                trial_id: the lead organization study id for this trial
+                metadata: a partial metadata object for trial_id
+
+            TODO: implement metadata merging, either here or in cidc_schemas
+        """
+        session = app.data.driver.session
+
+        # Look for an existing trial
+        trial = session.query(cls).filter_by(trial_id=trial_id).first()
+
+        if trial:
+            # Merge-update metadata into existing trial's metadata_json
+            raise NotImplementedError("metadata updates not yet supported")
+        else:
+            # Create a new trial metadata record, since none exists
+            app.logger.info(f"Creating new trial_metadata for trial {trial_id}")
+            new_trial = TrialMetadata(trial_id=trial_id, metadata_json=metadata)
+
