c6a0eba1ce5568ff222e49192c2ee97f1999dd6a
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-11-05 16:56:05 -0500

    Add template download endpoint

--- cidc_api/services/info.py
+++ cidc_api/services/info.py
@@ -1,5 +1,9 @@
 """Endpoints providing info related to this API"""
-from flask import Blueprint, jsonify
+import os
+import re
+
+from flask import Blueprint, jsonify, current_app as app, send_file
+from werkzeug.exceptions import NotFound, BadRequest
 
 from cidc_schemas import prism
 
@@ -30,3 +34,30 @@ def manifests():
 def extra_data_types():
     """List all extra data types on which permissions can be granted"""
     return jsonify(EXTRA_DATA_TYPES)
+
+
+alphabetic = re.compile(r"^[a-zA-Z]*$")
+
+
+@info_api.route("templates/<template_family>/<template_type>", methods=["GET"])
+def templates(template_family, template_type):
+    """
+    Return the empty Excel template file for the given 
+    `template_family` (e.g., manifests, metadata) and 
+    `template_type` (e.g., pbmc, olink).
+    """
+    # Check that both strings are alphabetic
+    if not alphabetic.match(template_family):
+        raise BadRequest(f"Invalid template family: {template_family}")
+    elif not alphabetic.match(template_type):
+        raise BadRequest(f"Invalid template type: {template_type}")
+
+    path = os.path.join(
+        app.config["TEMPLATES_DIR"], template_family, f"{template_type}_template.xlsx"
+    )
+
+    # Check that the template exists
+    if not os.path.exists(path):
+        raise NotFound(f"No {template_family} template exists for {template_type}")
+
+    return send_file(path)
