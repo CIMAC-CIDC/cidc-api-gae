44dc1e9040a231f6cd6489c7425efa5e8f2a14ac
Jacob Lurye <jlurye@jimmy.harvard.edu>
2019-11-15 15:47:08 -0500

    Add cidc_api.util.migrations module

--- migrations/versions/ff3141aecdd4_.py
+++ migrations/versions/ff3141aecdd4_.py
@@ -8,8 +8,7 @@ Create Date: 2019-11-14 11:43:49.297901
 import os
 
 from cidc_schemas.migrations import v0_10_0_to_v0_10_2
-
-from cidc_api.migration_utils import run_metadata_migration
+from cidc_api.utils.migrations import run_metadata_migration
 
 # revision identifiers, used by Alembic.
 revision = "ff3141aecdd4"
