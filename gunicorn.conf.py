cc771343d8aa843d2286fce89c8e9e458a20330e
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-04-22 16:04:36 -0400

    Fix app engine configs

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 from multiprocessing import cpu_count
 
 bind = f':{os.environ.get("PORT", 8080)}'
-loglevel = "DEBUG"
+loglevel = "INFO"
 timeout = 60
 # See https://docs.gunicorn.org/en/stable/design.html#how-many-workers
 workers = cpu_count() * 2 + 1
