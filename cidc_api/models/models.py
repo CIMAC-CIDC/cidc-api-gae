305a48374f634ed881c8ff917cfc37301cb14bac
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-07-10 10:13:15 -0400

    Require the upload_job.token field and generate UUIDs postgres-side

--- cidc_api/models/models.py
+++ cidc_api/models/models.py
@@ -1,5 +1,4 @@
 import os
-import uuid
 import json
 import hashlib
 from datetime import datetime, timedelta
@@ -34,8 +33,8 @@ from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.query import Query
-from sqlalchemy.sql import expression
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.sql import expression, text
+from sqlalchemy.dialects.postgresql import JSONB, UUID
 from sqlalchemy.engine.interfaces import ExecutionContext
 
 from cidc_schemas import prism, unprism, json_validation
@@ -563,7 +562,7 @@ class UploadJobs(CommonColumns):
         "status", Enum(*UPLOAD_STATUSES, name="upload_job_status"), nullable=False
     )
     # A long, random identifier for this upload job
-    token = Column(String, default=uuid.uuid4)
+    token = Column(UUID, server_default=text("gen_random_uuid()"), nullable=False)
     # Text containing feedback on why the upload status is what it is
     status_details = Column(String, nullable=True)
     # Whether the upload contains multiple files
