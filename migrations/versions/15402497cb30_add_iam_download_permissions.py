6dbad60c3e42f2caa3aff1d398cb072e6be83a60
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-09-09 18:12:53 -0400

    Consolidate IAM download permissions using '||'

--- migrations/versions/15402497cb30_add_iam_download_permissions.py
+++ migrations/versions/15402497cb30_add_iam_download_permissions.py
@@ -8,7 +8,7 @@ Create Date: 2020-08-28 13:04:44.756182
 from alembic import op
 import sqlalchemy as sa
 
-from cidc_api.models import Permissions
+# from cidc_api.models import Permissions
 
 # revision identifiers, used by Alembic.
 revision = "15402497cb30"
@@ -18,10 +18,13 @@ depends_on = None
 
 
 def upgrade():
-    session = sa.orm.session.Session(bind=op.get_bind())
-    Permissions.grant_all_iam_permissions(session=session)
+    # NOTE: since the IAM grant implementation has changed, we no longer run this code
+    # session = sa.orm.session.Session(bind=op.get_bind())
+    # Permissions.grant_all_iam_permissions(session=session)
+    pass
 
 
 def downgrade():
-    session = sa.orm.session.Session(bind=op.get_bind())
-    Permissions.revoke_all_iam_permissions(session=session)
+    # session = sa.orm.session.Session(bind=op.get_bind())
+    # Permissions.revoke_all_iam_permissions(session=session)
+    pass
