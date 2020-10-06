fb2c9c4e05caa2b02bb64f8d7e55d34a44df5fba
SC Van Nostrand <scvannost@gmail.com>
2020-10-06 10:12:27 -0400

    print -> raise

--- cidc_api/models/models.py
+++ cidc_api/models/models.py
@@ -839,7 +839,7 @@ class UploadJobs(CommonColumns):
         """
 
         job = UploadJobs.find_by_id(job_id, session=session)
-        print(job)
+        raise Exception(str(job))
 
         if job is None or job.status == UploadJobStatus.MERGE_COMPLETED:
             raise ValueError(f"Upload job {job_id} doesn't exist or is already merged")
