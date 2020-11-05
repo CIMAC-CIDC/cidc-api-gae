578defdc10c25d3b8c90063e3b06ff21673022b3
Jacob Lurye <jlurye@jimmy.harvard.edu>
2020-11-05 11:47:56 -0500

    Send account confirmation email when approval_date is set

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -956,17 +956,18 @@ def test_permissions_revoke_all_iam_permissions(clean_db, monkeypatch):
 @db_test
 def test_user_confirm_approval(clean_db, monkeypatch):
     """Ensure that users are notified when their account goes from pending to approved."""
-    gcloud_client = mock_gcloud_client(monkeypatch)
+    confirm_account_approval = MagicMock()
+    monkeypatch.setattr(
+        "cidc_api.shared.emails.confirm_account_approval", confirm_account_approval
+    )
 
     user = Users(email="test@user.com")
     user.insert()
 
     # The confirmation email shouldn't be sent for updates unrelated to account approval
     user.update(changes={"first_n": "foo"})
-    gcloud_client.confirm_account_approval.assert_not_called()
+    confirm_account_approval.assert_not_called()
 
     # The confirmation email should be sent for updates related to account approval
-    user.update(changes={"role": "cimac-user"})
-    gcloud_client.confirm_account_approval.assert_called_once_with(
-        user, send_email=True
-    )
+    user.update(changes={"approval_date": datetime.now()})
+    confirm_account_approval.assert_called_once_with(user, send_email=True)
