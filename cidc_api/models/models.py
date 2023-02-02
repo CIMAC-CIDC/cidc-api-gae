848fb25772906135ccefdf9737590dfc58a7fd49
JamesProvencher <jamesa.provencher@gmail.com>
2023-02-02 15:00:23 -0500

    CIDC-1619 remove some complexity

--- cidc_api/models/models.py
+++ cidc_api/models/models.py
@@ -951,11 +951,10 @@ class Permissions(CommonColumns):
                 grant_lister_access(user.email)
 
         if upload.upload_type in prism.SUPPORTED_MANIFESTS:
-            if upload.upload_type in prism.SUPPORTED_SHIPPING_MANIFESTS:
-                # Passed with empty user email list because they will be queried for in CFn
-                grant_download_access([], upload.trial_id, "participants info")
-                grant_download_access([], upload.trial_id, "samples info")
-        else:
+            # Passed with empty user email list because they will be queried for in CFn
+            grant_download_access([], upload.trial_id, "participants info")
+            grant_download_access([], upload.trial_id, "samples info")
+        elif upload.upload_type not in prism.SUPPORTED_WEIRD_MANIFESTS:
             grant_download_access(user_email_list, upload.trial_id, upload.upload_type)
 
     @staticmethod
